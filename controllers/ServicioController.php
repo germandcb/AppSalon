2197e3323b59d48c908cbbc0b384d8bd59dd2110
germandc <germancelis16@gmail.com>
2024-05-19 17:48:14 -0500

    Creando vistas para los servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -7,21 +7,37 @@ use MVC\Router;
 class ServicioController {
 
     public static function index (Router $router) {
-        $router->render('servicios/index', [
 
+        session_start();
+        $router->render('servicios/index', [
+            'nombre' => $_SESSION['nombre']
         ]);
     }
 
     public static function crear (Router $router) {
+
+        session_start();
+
         if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
         }
+
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function actualizar (Router $router) {
+
+        session_start();
+
         if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
             
         }
+
+        $router->render('servicios/actualizar', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function eliminar (Router $router) {
